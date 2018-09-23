48a025f4fe6f6e361c60429c4eb0bc01e1052661
Jeffrey Parrish <jeffs.linux@gmail.com>
2018-09-23 16:54:38 -0400

    added authentication!

--- serve.js
+++ serve.js
@@ -1,28 +1,84 @@
 const express = require('express');
-const app = require('./app/app.js');
 const multer = require('multer');
-const upload = multer({dest: process.env.UPLOAD_DIRECTORY});
+const passport = require("passport");
+const local = require('passport-local').Strategy;
+const app = require('./app/app.js');
+const jwt = require('jsonwebtoken');
+const md5 = require('blueimp-md5');
+const passportJWT = require("passport-jwt");
 
 require('dotenv').config();
+
+const JWTStrategy   = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+const upload = multer({dest: process.env.UPLOAD_DIRECTORY});
+let pattern = '\/[A-Za-z\/_]+';
+let expressApp = express();
 
 let dispatch = (req,res) => {
 	console.log("dispatching " + req.method + " " + req.path);
 	return app.new().dispatch(req,res);
-}
-let pattern = '\/[A-Za-z\/_]+';
-let expressApp = express();
+};
 
-expressApp.get('/' , (request,response) => {response.send("hello world");});
-expressApp.post('/import/importFile', upload.single('transactions'), (req,res) => {
+let AuthMethod = function(req,res){
+	return passport.authenticate('local', {session: false}, (err, user, info) => {
+		if (err || !user) {
+			return res.status(400).json({
+				message: 'Something is not right',
+				user   : user
+			});
+		}
+		req.login(user, {session: false}, (err) => {
+			if (err) res.send(err);
+			// generate a signed son web token with the contents of user object and return it in the response
+			const token = jwt.sign(user, process.env.JWT_SECRET,{expiresIn:'1w'});
+			return res.json({user, token});
+		});
+	})(req, res);
+};
+
+passport.use(new local({},(user,pass,callback) => {
+	if(user !== process.env.PRIMARY_USERNAME) 
+		return callback(null,false);
+	if(md5(pass) !== process.env.PRIMARY_PASSWORD)
+		return callback(null,false);
+	return callback(null,{
+		id: 1, username: process.env.PRIMARY_USER
+	});
+}));
+
+passport.use(new JWTStrategy({
+		jwtFromRequest: ExtractJWT.fromUrlQueryParameter('authorization'),
+		secretOrKey   : process.env.JWT_SECRET
+	},
+	(jwtPayload, cb) => {
+		if(jwtPayload.id === 1){
+			return cb(null,{});
+		}
+		return cb(unauthorized)
+	}
+));
+
+
+expressApp.post('/login', (req,res,next) => {
+	AuthMethod(req,res);
+});
+
+let appRouter = express.Router();
+expressApp.use('/v1',passport.authenticate('jwt',{session:false}),appRouter);
+appRouter.get('/' , (request,response,next) => {response.send("hello world");});
+appRouter.post('/Import/ImportFile', upload.single('transactions'), (req,res) => {
 	console.log("upload triggered");
 	return dispatch(req,res);
 });
 
-expressApp.get(pattern, dispatch);
-expressApp.post(pattern, dispatch);
-expressApp.put(pattern, dispatch);
-expressApp.patch(pattern, dispatch);
+appRouter.get(pattern, dispatch);
+appRouter.post(pattern, dispatch);
+appRouter.put(pattern, dispatch);
+appRouter.patch(pattern, dispatch);
 
 expressApp.listen(process.env.EXPRESS_PORT, () => {
 	console.log("Serving on " + process.env.EXPRESS_PORT)
 });
+
+{% response 'body', 'req_53b656aac32f42a599a28e78abc7bd54', '$.token' %}
