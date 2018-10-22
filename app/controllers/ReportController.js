6de42b17111f76fc5fdb08e4aa3015e64cd0ff00
Jeffrey Parrish <jeffs.linux@gmail.com>
2018-10-22 09:35:50 -0400

    renaming 'run' command to just 'start'

--- app/controllers/ReportController.js
+++ app/controllers/ReportController.js
@@ -1,4 +1,4 @@
-const Sequelize = require("Sequelize");
+const Sequelize = require("sequelize");
 const db = require("../lib/connection.js");
 module.exports = class ReportController{
 	getTotalByMonth(request,response){
